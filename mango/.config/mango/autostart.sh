9309c9de823f11f9df4947d7aed81d754d4d8e11
Simpaqt <simpa@proton.me>
2025-10-23 19:42:51 +0200

    feat: added wallpaper folder

--- mango/.config/mango/autostart.sh
+++ mango/.config/mango/autostart.sh
@@ -1,2 +1,2 @@
 waybar -c ~/.config/mango/config.jsonc -s ~/.config/mango/style.css >/dev/null 2>&1 &
-swaybg -i ~/Downloads/jecgsqy4lztf1.jpeg >/dev/null 2>&1 &
+swaybg -i /home/simpa/walls/jecgsqy4lztf1.jpeg >/dev/null 2>&1 &
